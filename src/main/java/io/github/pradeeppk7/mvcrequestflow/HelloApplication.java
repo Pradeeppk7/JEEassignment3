1ad0e2efdc702431df9380af06af9155861fa714
Pradeeppk7 <pradeepkumar.pk002@gmail.com>
2026-04-08 19:14:29 -0500

    Initial commit

---
+++ src/main/java/io/github/pradeeppk7/mvcrequestflow/HelloApplication.java
@@ -0,0 +1,9 @@
+package io.github.pradeeppk7.mvcrequestflow;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
